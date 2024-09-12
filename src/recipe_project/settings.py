342c913c1f177af39f2ea144f3fea1052bf0dd77
rkanishka <143039356+rkanishka@users.noreply.github.com>
2024-09-12 14:45:11 -0700

    Add files via upload

--- src/recipe_project/settings.py
+++ src/recipe_project/settings.py
@@ -9,7 +9,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY ='django-insecure-795v6t%e3_)@b_9h!nvgagn_i)wxm$3sf1+ddb(w)n=d68ck=w'
+ 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
