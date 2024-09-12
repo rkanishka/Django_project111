eaa4dffe3b3bf6a6e60f3fab47917bb0d6af768f
rkanishka <143039356+rkanishka@users.noreply.github.com>
2024-09-12 15:43:03 -0700

    Add files via upload

--- src/recipe_project/settings.py
+++ src/recipe_project/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-795v6t%e3_)@b_9h!nvgagn_i)wxm$3sf1+ddb(w)n=d68ck=w')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['myrecipe111.herokuapp.com', 'localhost', '127.0.0.1']
 
